39518c4ae7198045553fdadf16a607836bcbc137
brettof86 <bjones027@gmail.com>
2012-12-15 22:31:21 -0600

    Significantly increased the speed of 4

--- 4.rb
+++ 4.rb
@@ -2,12 +2,13 @@
 # A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 99.
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
-ans = (100..999).map{ |a|
-  (100..999).map{ |b|
+range = (100..999)
+
+puts range.flat_map{ |a|
+  range.map{ |b|
     a*b
   }
-}.flatten.select{ |p|      # select those, which are reversed
-  p.to_s == p.to_s.reverse # the same as non-reversed
-}.sort.reverse.first
-
-puts ans
+}.find_all{ |p|
+  s = p.to_s
+  s == s.reverse
+}.max
